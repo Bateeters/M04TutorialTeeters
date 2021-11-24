e464cca52fce3cf61b4fe2f44cf7b68ba406f3ab
Brian Teeters <91576990+Bateeters@users.noreply.github.com>
2021-11-24 00:36:32 +0000

    status codes

--- script.js
+++ script.js
@@ -11,12 +11,15 @@ const server = http.createServer((req, res) => {
   switch(req.url) {
     case '/':
       path += 'index.html'
+      res.statusCode = 200;
       break;
     case '/about':
       path += 'about.html';
+      res.statusCode = 200;
       break;
     default:
       path += '404.html';
+      res.statusCode = 404;
       break;
   }
 
@@ -27,6 +30,7 @@ const server = http.createServer((req, res) => {
       res.end();
     } else {
       //res.write(data);
+
       res.end(data);
     }
   })
