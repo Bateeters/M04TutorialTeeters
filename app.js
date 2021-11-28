df6f4c1276e6475f3e8a0cc15f91dd6c4fc5cb2c
Brian Teeters <brianteetersdesign@gmail.com>
2021-11-28 16:01:28 -0500

    passing data into views

--- app.js
+++ app.js
@@ -11,21 +11,24 @@ app.listen(3000);
 
 
 app.get('/', (req, res) => {
-    //res.send('<p>home page</p>');
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'lorem ipsum dolor sit'},
+        {title: 'Mario finds stars', snippet: 'lorem ipsum dolor sit'},
+        {title: 'How to defeat bowser', snippet: 'lorem ipsum dolor sit'}
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    //res.send('<p>about page</p>');
-    res.render('about');
+    res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req,res) => {
-    res.render('create');
+    res.render('create', { title: 'Create a new Blog' });
 });
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404' });
 });
 
